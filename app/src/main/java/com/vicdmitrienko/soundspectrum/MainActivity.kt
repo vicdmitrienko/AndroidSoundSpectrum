af59171f606e658699f515452718e3c4a93c763f
Victor Dmitrienko <vicdmitrienko@gmail.com>
2022-04-17 01:02:09 +0100

    Записал первые задачи

--- app/src/main/java/com/vicdmitrienko/soundspectrum/MainActivity.kt
+++ app/src/main/java/com/vicdmitrienko/soundspectrum/MainActivity.kt
@@ -3,7 +3,10 @@ package com.vicdmitrienko.soundspectrum
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//
+// MVP
+//TODO: Научиться получать звук с микрофона устройства
+//TODO: Сделать View для отображения спектра на экране
+//TODO: Научиться выделать и определять из спектра ноты
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
